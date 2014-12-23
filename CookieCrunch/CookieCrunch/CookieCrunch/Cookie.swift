cbf56b1d68698d1ea7e58f972d92024a79bdcb04
Alan Liang <alantliang@gmail.com>
2014-12-22 21:05:13 -0500

    Can I have some Cookies?
    Completed enum and cookie model sections

---
+++ CookieCrunch/CookieCrunch/CookieCrunch/Cookie.swift
@@ -0,0 +1,45 @@
+//
+//  Cookie.swift
+//  CookieCrunch
+//
+//  Created by Alan Liang on 12/22/14.
+//  Copyright (c) 2014 fsa. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+enum CookieType: Int {
+    case Unknown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SUgarCookie
+    
+    var spriteName: String {
+        let spriteNames = [
+            "Croissant",
+            "Cupcake",
+            "Danish",
+            "Donut",
+            "Macaroon",
+            "SugarCookie"]
+        return spriteNames[rawValue - 1]
+    }
+    
+    var highlightedSpriteName: String {
+        return spriteName + "-Highlighted"
+    }
+    static func random() -> CookieType {
+        return CookieType(rawValue: Int(arc4random_uniform(6)) + 1)!  // what does this exclamation mean?
+    }
+}
+
+class Cookie {
+    var column: Int
+    var row: Int
+    let cookieType: CookieType
+    var sprite: SKSpriteNode?
+    
+    init(column: Int, row: Int, cookieType: CookieType) {
+        self.column = column
+        self.row = row
+        self.cookieType = cookieType
+    }
+}
