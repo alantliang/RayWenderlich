552df326dc8202f7b8a104e7b600a1b6ad3fff99
Alan Liang <alantliang@gmail.com>
2015-01-19 21:32:44 -0500

    How To Create a Breakout Game, SpriteKit + Swift

---
+++ BreakoutSpriteKitTutorial/BreakoutSpriteKitTutorial/BreakoutSpriteKitTutorial/GameScene.swift
@@ -0,0 +1,12 @@
+import SpriteKit
+
+let BallCategoryName = "ball"
+let PaddleCategoryName = "paddle"
+let BlockCategoryName = "block"
+let BlockNodeCategoryName = "blockNode"
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        super.didMoveToView(view)
+    }
+}
